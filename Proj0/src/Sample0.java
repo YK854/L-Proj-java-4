78e4d184e9cdac9655084ab510cdff8602ac97aa
YK854 <95330893+YK854@users.noreply.github.com>
2022-05-05 21:56:57 +0900

    [fix]不要な空白削除

--- Proj0/src/Sample0.java
+++ Proj0/src/Sample0.java
@@ -12,12 +12,12 @@ public class Sample0 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		for (int i=0; i<=5; i++) {			
+		for (int i=0; i<=5; i++) {	
 			System.out.println("繰り返し" + i + "回");
 		}
 		for(int i=0; i<=6; i++) {
-			System.out.println(i+i);		}
+			System.out.println(i+i);
+		}
 	}
 
 }
