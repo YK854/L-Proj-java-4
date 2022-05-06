ae96d986fd16d2bd5341505be7ad6b3f023f614b
YK854 <95330893+YK854@users.noreply.github.com>
2022-05-06 10:57:31 +0900

    [add]学習コンテンツ1章から4章

---
+++ L-Proj-java-0/src/Ready02.java
@@ -0,0 +1,33 @@
+
+public class Ready02 {
+	public static void main(String[] args) {
+		// Java基礎コンテンツ
+		// 学習コンテンツ1章から3章
+		System.out.println("Hello WEBCAMP");
+		System.out.println("Hello Java!");
+
+		System.out.println("");
+
+		// 学習コンテンツ4章
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello Word!";
+
+		System.out.println(num1);
+		System.out.println(str1);
+
+		num1 = 20;
+		System.out.println(num1);
+
+		int num2 = 30;
+		System.out.println(num2);
+
+		System.out.println("");
+		
+		//定数
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+		
+	}
+}
